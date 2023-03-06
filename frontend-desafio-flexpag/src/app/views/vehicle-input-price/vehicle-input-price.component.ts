45700782259dfc3ae28c39ade021a547bf1eba3e
graciellenascimento <grazciellenascimento@gmail.com>
2023-03-06 09:11:03 -0300

    cálculo

--- frontend-desafio-flexpag/src/app/views/vehicle-input-price/vehicle-input-price.component.ts
+++ frontend-desafio-flexpag/src/app/views/vehicle-input-price/vehicle-input-price.component.ts
@@ -9,14 +9,26 @@ import { VehicleDataComponent } from '../vehicle-data/vehicle-data.component';
 })
 export class VehicleInputPriceComponent {
 
-  @Input() testeinput!: any 
+  @Input() testeinput!: any //recebe o valor do veículo do arquivo vehicle-data para fazer o cálculo
+
+  testeCalculo!: number
+  testenumber!: string
+  convertToNumber!: number
 
   onSubmit(f: NgForm){
-    console.log(5+2)
+    console.log(f.value.price)
+    console.log(parseFloat(f.value.price))
+    
+    this.testenumber = this.testeinput.replace('R$ ', '').replace('.','') //retira o R$ e o ponto do valor recebido pelo vehicle-data
+    this.convertToNumber = parseFloat(this.testenumber) //transforma o valor em número para poder realizar o cálculo
+    this.testeCalculo = ((parseFloat(f.value.price)-this.convertToNumber)/this.convertToNumber)*100 //cálculo para retirar média do valor de acordo com a tabela FIPE
+    console.log(this.convertToNumber)
     console.log(this.testeinput)
+    console.log(this.convertToNumber+1000)
+    console.log('media tabela fipe: ' + this.testeCalculo)
   }
 
-  constructor(private dados: VehicleDataComponent) {}
+  constructor() {}
 
 
   ngOnInit(): void {
